22c2495b4066e17a6859a6bbc2483fda88e73010
Ricardo Noronha de Menezes <ricardo.noronha@outlook.com>
2025-03-08 17:06:19 -0300

    feat: customer list initial layout

---
+++ BlazorLoginSocial.Domain/Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorLoginSocial.Domain.Entities;
+
+public class Customer
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string TownName { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+
+    public string Febraban { get; set; } = string.Empty;
+
+}
